2bde1c90396c7fd6a911bcf132414185553e1ede
Pavlo Podolian <pavlikpodolyan7525@gmail.com>
2025-02-07 17:32:57 +0100

    add pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {}
--- src/app/components/pages/shop/shop.component.ts
+++ src/app/components/pages/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { computed, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -16,14 +16,33 @@ import { ProductCardComponent } from '../../product-card/product-card.component'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ShopComponent implements OnInit {
-  public currentTypeOfSorting = signal<TypesOfSorting>('asc');
-  public isActiveSortButtons = signal(true);
   private router = inject(Router);
   private activeRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
   private productsService = inject(ProductsService);
+
   public currentChildRoute = signal<string>('all-products');
+  public currentTypeOfSorting = signal<TypesOfSorting>('asc');
+
+  public isActiveSortButtons = signal(true);
+
   public productData = signal<undefined | ProductDescription[]>(undefined);
+
+  public availablePages = signal(1);
+  public arrayOfPages = computed<number[]>(() => {
+    let arr = [];
+    if (this.availablePages() <= 5 || this.currentPage() < 2)
+      for (let i = 1; i <= this.availablePages(); i++) {
+        arr.push(i);
+      }
+    else
+      for (let i = this.currentPage() - 2; i <= 5; i++) {
+        arr.push(i);
+      }
+
+    return arr;
+  });
+  public currentPage = signal(0);
   ngOnInit(): void {
     const sortingType = this.activeRoute.snapshot.queryParams['sort_by'];
     if (sortingType) this.currentTypeOfSorting.set(sortingType);
@@ -31,16 +50,24 @@ export class ShopComponent implements OnInit {
       .getProductData()
       .subscribe((data) => {
         this.productData.set(data.content);
+        this.availablePages.set(data.totalPages);
+        this.currentPage.set(data.pageable.pageNumber);
       });
     const paramsSubscription = this.activeRoute.params.subscribe(
       ({ typeCategory }) => {
-        this.productData.set([]);
+        this.currentChildRoute.set(typeCategory);
         this.productsService.typeOfCategory$.next(typeCategory);
         this.currentChildRoute.set(typeCategory);
       }
     );
     const queryParamsSubscription = this.activeRoute.queryParams.subscribe(
       ({ sort_by }) => {
+        if (!sort_by && this.currentChildRoute() !== 'all-products') {
+          this.router.navigate(['./'], {
+            queryParams: { sort_by: this.currentTypeOfSorting() },
+            relativeTo: this.activeRoute,
+          });
+        }
         this.productsService.typeOfSorting$.next(sort_by);
       }
     );
@@ -50,14 +77,16 @@ export class ShopComponent implements OnInit {
       queryParamsSubscription.unsubscribe();
     });
   }
-
+  onChangePage(page: number) {
+    this.productsService.pageOfProduct$.next(page);
+    this.currentPage.set(page);
+  }
   onChangeType(type: TypesOfSorting) {
-    this.productData.set([]);
     this.currentChildRoute.set(
       this.activeRoute.snapshot.params['typeCategory']
     );
     this.currentTypeOfSorting.set(type);
-    this.router.navigate(['/shop', this.currentChildRoute()], {
+    this.router.navigate(['./'], {
       queryParams: { sort_by: this.currentTypeOfSorting() },
       relativeTo: this.activeRoute,
     });
