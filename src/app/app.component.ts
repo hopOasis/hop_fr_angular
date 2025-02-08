959c4b9205d8294dcb54a8910f3112e12dcb43e5
Vitiok2021 <viktorpts1vol@gmail.com>
2025-02-08 15:10:01 +0200

    add new product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 
 import { FooterComponent } from './components/footer/footer.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductDescription } from './models/pageable.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
 
-  imports: [HeaderComponent, RouterOutlet, FooterComponent],
+  imports: [
+    HeaderComponent,
+    RouterOutlet,
+    FooterComponent,
+    ProductCardComponent,
+  ],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  myProduct!: ProductDescription;
+}
--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,14 +1,24 @@
 import { Component, input } from '@angular/core';
 import { ProductDescription } from '../../models/pageable.model';
 import { ChangeDetectionStrategy } from '@angular/core';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductCardComponent {
   public data = input.required<ProductDescription>();
+  myProduct: any = {
+    id: 1,
+    image: 'product-cards/01.png',
+    title: 'Пиво темне з фруктовими нотками English IPA, 6%',
+    volumes1: 0.33,
+    volumes2: 0.5,
+    price: 70,
+    stock: true, // true - в наявності, false - немає
+  };
 }
