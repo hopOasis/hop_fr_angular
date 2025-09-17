051a03e9d3d27a1f10136655d70e64499fee66d5
VolodumurGruv <gru.v@i.ua>
2025-09-17 21:09:55 +0300

    picture added

--- src/app/home/page-2/page-2.component.spec.ts
+++ src/app/home/page-2/page-2.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-<<<<<<<< HEAD:src/app/home/page-2/page-2.component.spec.ts
 import { Page2Component } from './page-2.component';
 
 describe('Page2Component', () => {
@@ -9,26 +8,11 @@ describe('Page2Component', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Page2Component]
-    })
-    .compileComponents();
+      imports: [Page2Component],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Page2Component);
-========
-import { NotFoundComponent } from './not-found.component';
 
-describe('NotFoundComponent', () => {
-  let component: NotFoundComponent;
-  let fixture: ComponentFixture<NotFoundComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [NotFoundComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(NotFoundComponent);
->>>>>>>> 6bc1499 (cart functionality):src/app/shared/layout/not-found/not-found.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
