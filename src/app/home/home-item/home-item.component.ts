71b2b180ac05cee759b49ff84ae21a280f110292
VolodumurGruv <gru.v@i.ua>
2025-09-18 21:51:28 +0300

    added image optimizer and responsive button

--- src/app/home/home-item/home-item.component.ts
+++ src/app/home/home-item/home-item.component.ts
@@ -1,15 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home-item',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './home-item.component.html',
   styleUrl: './home-item.component.scss',
 })
 export class HomeItemComponent {
   private router = inject(Router);
+
+  constructor() {}
 
   redirectToShop() {
     this.router.navigate(['/shop']);
