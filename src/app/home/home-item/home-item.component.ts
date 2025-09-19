130360ded5f2d0e0505ee57e140a6656b5460516
VolodumurGruv <gru.v@i.ua>
2025-09-20 01:18:49 +0300

    made css more smooth adaptivnes

--- src/app/home/home-item/home-item.component.ts
+++ src/app/home/home-item/home-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home-item',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './home-item.component.html',
   styleUrl: './home-item.component.scss',
 })
