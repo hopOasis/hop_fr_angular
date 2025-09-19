b23a2b8e8974a7e711144e8497936c490e0dd787
VolodumurGruv <gru.v@i.ua>
2025-09-19 21:41:43 +0300

    deleted unnecessary import

--- src/app/home/page-1/data-access/services/active-offers.service.ts
+++ src/app/home/page-1/data-access/services/active-offers.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
-import { map, Observable, switchMap } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 import { environment } from '../../../../environments/environment';
 import { Offer } from '../models/offer.interface';
