c6e462dcc8ec4c9522a99005f22a29a9e2360808
VolodumurGruv <gru.v@i.ua>
2025-09-23 21:48:12 +0300

    content added

--- src/app/home/page-2/page-2.component.ts
+++ src/app/home/page-2/page-2.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-page-2',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './page-2.component.html',
-  styleUrl: './page-2.component.scss'
+  styleUrl: './page-2.component.scss',
 })
-export class Page2Component {
-
-}
+export class Page2Component {}
