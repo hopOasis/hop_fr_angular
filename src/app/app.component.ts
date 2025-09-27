479df786f546400f12c12f7cee493573fd4daa65
VolodumurGruv <gru.v@i.ua>
2025-09-28 00:21:10 +0300

    starting template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
 import { AuthModalComponent } from './authentication/feature/auth-modal/auth-modal.component';
 import { CartModalComponent } from './cart/feature/cart-modal/cart-modal.component';
 import { AuthApiService } from './authentication/data-access/api/auth-api.service';
+import { AgeConfirmationComponent } from './age-confirmation/age-confirmation.component';
 
 @Component({
   selector: 'app-root',
@@ -15,9 +21,11 @@ import { AuthApiService } from './authentication/data-access/api/auth-api.servic
     FooterComponent,
     AuthModalComponent,
     CartModalComponent,
+    AgeConfirmationComponent,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',changeDetection:ChangeDetectionStrategy.OnPush
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
   readonly authApi = inject(AuthApiService);
--- src/app/home/page-1/data-access/services/active-offers.service.ts
+++ src/app/home/page-1/data-access/services/active-offers.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 import { catchError, map, Observable } from 'rxjs';
@@ -6,11 +6,24 @@ import { catchError, map, Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { Offer } from '../models/offer.interface';
 import { ProductDescription } from '../../../../catalog/data-access/models/product-description.model';
+import { FetchedProductData } from '../../../../catalog/data-access/models/product-api-response.model';
 
 @Injectable()
 export class ActiveOffersService {
   private httpClient = inject(HttpClient);
   private readonly url = environment.apiUrl;
+  /*
+  // @dev testing purpose
+  getActiveOffers(prodName: string): Observable<ProductDescription[] | null> {
+    return this.httpClient.get<FetchedProductData>(`${this.url}/beers`).pipe(
+      map((item) => item.content),
+      catchError((err: HttpErrorResponse) => {
+        throw `Error in active offers on product name: ${prodName}. Details: ${err.message}`;
+      })
+    );
+  }
+
+  */
 
   getActiveOffers(prodName: string): Observable<ProductDescription[] | null> {
     return this.httpClient.get<Offer[]>(`${this.url}/special-offers`).pipe(
