afe88e41bbd0a41c578579391a89df3fb85c9c25
VolodumurGruv <gru.v@i.ua>
2025-09-29 00:30:05 +0300

    age confirmation is ready for testing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,22 @@ import { Routes } from '@angular/router';
 import { shopRoutes } from './catalog/feature/product.routes';
 import { checkUserPermission } from './authentication/data-access/guards/auth.guard';
 import { userCabinetRoutes } from './user/feature/user-cabinet.routes';
+import { ageGuard } from './age-confirmation/data-access/age.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'home',
+    canActivate: [ageGuard],
     loadComponent: () =>
       import('./home/home.component').then((m) => m.HomeComponent),
   },
 
-  { path: 'shop', children: shopRoutes },
+  { path: 'shop', canActivate: [ageGuard], children: shopRoutes },
   {
     path: 'my_cabinet',
     canMatch: [checkUserPermission],
+    canActivate: [ageGuard],
     children: userCabinetRoutes,
     loadComponent: () =>
       import('./user/feature/my-cabinet/my-cabinet.component').then(
@@ -27,5 +30,12 @@ export const routes: Routes = [
       import('./shared/layout/not-found/not-found.component').then(
         (m) => m.NotFoundComponent
       ),
+  },
+  {
+    path: 'confirmage',
+    loadComponent: () =>
+      import(
+        './age-confirmation/feauters/confirmation/age-confirmation.component'
+      ).then((c) => c.AgeConfirmationComponent),
   },
 ];
