ee81ef464939a1efd98b4ae190d7d23cd82913da
VolodumurGruv <gru.v@i.ua>
2025-10-09 18:47:39 +0300

    deleted imports to deleted folders

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,17 +3,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Page1Component } from './page-1/features/page-1.component';
 import { HomeItemComponent } from './home-item/home-item.component';
 import { Page2Component } from './page-2/page-2.component';
-import { TeamRecommendationsSectionComponent } from '../team-recommendations-section/team-recommendations-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    Page1Component,
-    HomeItemComponent,
-    Page2Component,
-    TeamRecommendationsSectionComponent,
-  ],
+  imports: [Page1Component, HomeItemComponent, Page2Component],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
